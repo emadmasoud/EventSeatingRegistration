f5177423fa099af9b3aded55d3f110e3ddd3e37b
MemonaSultan <memonasultan54@gmail.com>
2020-03-15 23:13:00 +0500

    Register Page Completed with Backend integration

--- event-seating-registeration/src/App.js
+++ event-seating-registeration/src/App.js
@@ -4,7 +4,9 @@ import './App.css';
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 import RegistrationForm from "./RegistrationForm";
+import TermsAndCondition from "./TermsAndCondition";
 import SeatPicker from "./SeatPicker";
+import EventList from "./EventList";
 import { ToastsContainer, ToastsStore } from 'react-toasts';
 
 function App() {
@@ -12,14 +14,18 @@ function App() {
     <div className="App">
      <ToastsContainer store={ToastsStore}></ToastsContainer>
 
+     <Switch>
+     <Route exact path='/' component={TermsAndCondition} />
+     <Route exact path="/seatPicker" component={SeatPicker} />    
+     <Route exact path="/events" component={EventList} />    
+      
       <div className="auth-wrapper">
-        <div className="auth-inner">
-          <Switch>
-            <Route exact path='/' component={RegistrationForm} />
-            <Route path="/register" component={RegistrationForm} />
-          </Switch>
-        </div>
+        <div className="auth-inner">              
+        <Route exact path="/register" component={RegistrationForm} />      
+        </div>   
       </div>
+     
+      </Switch>
     </div></Router>
   );
 }
--- event-seating-registeration/src/RegistrationForm.js
+++ event-seating-registeration/src/RegistrationForm.js
@@ -3,6 +3,7 @@ import { Button, Form, Col, Row } from 'react-bootstrap';
 import SeatPicker from 'react-seat-picker'
 import { BASE_URL } from './Config'
 import { ToastsStore } from 'react-toasts';
+import { Redirect } from 'react-router-dom';
 export default class SeatPlanner extends Component {
   constructor(props) {
     super(props)
@@ -16,7 +17,8 @@ export default class SeatPlanner extends Component {
       c_password: '',
       class_no: 'Economic',
       agreed: false,
-      password_error_class: ''
+      password_error_class: '',
+      showSeatPickerButton: false
     }
   }
 
@@ -36,9 +38,17 @@ export default class SeatPlanner extends Component {
   }
 
   registerUser = e => {
-
     e.preventDefault();
-    let user = this.state;
+    let user = {
+      first_name: this.state.first_name,
+      middle_name: this.state.middle_name,
+      last_name: this.state.last_name,
+      class_no: this.state.class_no,
+      email: this.state.email,
+      phone: this.state.phone,
+      password: this.state.password
+
+    };
     fetch(BASE_URL + "register", {
       headers: {
         'Accept': 'application/json',
@@ -49,162 +59,176 @@ export default class SeatPlanner extends Component {
       body: JSON.stringify(user)
     }).then(data => {
       console.log(data)
-      if(data.status==200)
-      {
+      if (data.status == 200) {
         ToastsStore.success("Successfully Registered!")
-        window.location.href = 'http://localhost:3000/sign-in'
+        this.setState({ showSeatPickerButton: true })
       }
-      else
-      {
+      else {
         ToastsStore.error("Something went wrong! Try Again..")
+        this.setState({ showSeatPickerButton: false })
       }
     }).catch(err => {
       ToastsStore.error("Something went wrong! Try Again..")
+      this.setState({ showSeatPickerButton: false })
     })
   }
 
+  
+  goToTablePage = e =>{
+    return <Redirect to="/seatPicker"></Redirect>
+  }
+
+  registrationForm(){
+   return  <div>
+    <form>
+      <h3>Sign Up</h3>
+
+      <Row>
+        <Col>
+          <div className="form-group">
+            <label>First name</label>
+            <input type="text"
+              required
+              className="form-control"
+              placeholder="First name"
+              value={this.state.first_name}
+              name='first_name'
+              onChange={this.onFormValueChangeHandler} />
+          </div>
+        </Col>
+        <Col>
+          <div className="form-group">
+            <label>Middle name</label>
+            <input type="text"
+              required
+              className="form-control"
+              placeholder="Middle name"
+              value={this.state.middle_name}
+              name='middle_name'
+              onChange={this.onFormValueChangeHandler} />
+          </div>
+        </Col>
+        <Col>
+          <div className="form-group">
+            <label>Last name</label>
+            <input type="text"
+              required
+              className="form-control"
+              placeholder="Last name"
+              value={this.state.last_name}
+              name='last_name'
+              onChange={this.onFormValueChangeHandler} />
+          </div>
+        </Col>
+      </Row>
+
+
+      <Row>
+        <Col>
+          <div className="form-group">
+            <label>Email address</label>
+            <input type="email"
+              required
+              className="form-control"
+              placeholder="Enter email"
+              value={this.state.email}
+              name='email'
+              onChange={this.onFormValueChangeHandler} />
+          </div>
+        </Col>
+        <Col>
+          <div className="form-group">
+            <label>Phone No.</label>
+            <input type="number"
+              required
+              className="form-control"
+              placeholder="Enter phone number"
+              value={this.state.phone}
+              name='phone'
+              onChange={this.onFormValueChangeHandler} />
+          </div>
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <div className="form-group">
+            <label>Class</label>
+            <select
+              required
+              className="form-control"
+              name='class_no'
+              value={this.state.class_no}
+              onChange={this.onFormValueChangeHandler}
+            >
+
+              <option value="Business">Business</option>
+              <option value="Economic">Economic</option>
+            </select>
+          </div>
+
+
+        </Col>
+      </Row>
+
+      <Row>
+        <Col>
+          <div className="form-group">
+            <label>Password</label>
+            <input type="password"
+              required
+              className={"form-control " + this.state.password_error_class}
+              placeholder="Enter password"
+              value={this.state.password}
+              name='password'
+              onChange={this.onFormValueChangeHandler} />
+          </div>
+
+
+        </Col>
+        <Col>
+          <div className="form-group">
+            <label>Confirm Password</label>
+            <input type="password"
+              required
+              className={"form-control " + this.state.password_error_class}
+              placeholder="Enter password"
+              value={this.state.c_password}
+              name='c_password'
+              onChange={this.onFormValueChangeHandler}
+            />
+          </div>
+        </Col>
+      </Row>
+
+
+      <span>
+        <input type="checkbox"
+          required
+          checked={this.state.agreed}
+          name='agreed'
+          onChange={this.onFormValueChangeHandler} />
+        <span className="forgot-password text-left"> I agree to the <a href="#"> Terms & Conditions</a> mentioned by Event Planners</span>
+
+      </span>
+
+
+      <button type="submit" className="btn btn-primary btn-block mt-3" disabled={(!this.state.agreed || this.state.password_error_class != '')} onClick={this.registerUser}>Register</button>
+      <p className="forgot-password text-right">
+        Already registered <a href="#">sign in?</a>
+      </p>
+    </form>
+
+  </div>
+  }
+
+
   render() {
+    const pageContent = !this.state.showSeatPickerButton ? this.registrationForm():this.goToTablePage();
 
     return (
-      <form >
-        <h3>Sign Up</h3>
-        <container>
-          <Row>
-            <Col>
-              <div className="form-group">
-                <label>First name</label>
-                <input type="text"
-                  required
-                  className="form-control"
-                  placeholder="First name"
-                  value={this.state.first_name}
-                  name='first_name'
-                  onChange={this.onFormValueChangeHandler} />
-              </div>
-            </Col>
-            <Col>
-              <div className="form-group">
-                <label>Middle name</label>
-                <input type="text"
-                  required
-                  className="form-control"
-                  placeholder="Middle name"
-                  value={this.state.middle_name}
-                  name='middle_name'
-                  onChange={this.onFormValueChangeHandler} />
-              </div>
-            </Col>
-            <Col>
-              <div className="form-group">
-                <label>Last name</label>
-                <input type="text"
-                  required
-                  className="form-control"
-                  placeholder="Last name"
-                  value={this.state.last_name}
-                  name='last_name'
-                  onChange={this.onFormValueChangeHandler} />
-              </div>
-            </Col>
-          </Row>
-
-
-          <Row>
-            <Col>
-              <div className="form-group">
-                <label>Email address</label>
-                <input type="email"
-                  required
-                  className="form-control"
-                  placeholder="Enter email"
-                  value={this.state.email}
-                  name='email'
-                  onChange={this.onFormValueChangeHandler} />
-              </div>
-            </Col>
-            <Col>
-              <div className="form-group">
-                <label>Phone No.</label>
-                <input type="number"
-                  required
-                  className="form-control"
-                  placeholder="Enter phone number"
-                  value={this.state.phone}
-                  name='phone'
-                  onChange={this.onFormValueChangeHandler} />
-              </div>
-            </Col>
-          </Row>
-          <Row>
-            <Col>
-              <div className="form-group">
-                <label>Class</label>
-                <select 
-                  required
-                  className="form-control"
-                  name='class_no'
-                  value={this.state.class_no}
-                  onChange={this.onFormValueChangeHandler}
-                 >
-                
-                  <option value="Business">Business</option>
-                    <option value="Economic">Economic</option>                 
-                  </select>
-              </div>
-
-
-            </Col>
-            </Row>
-
-                      <Row>
-                        <Col>
-                          <div className="form-group">
-                            <label>Password</label>
-                            <input type="password"
-                              required
-                              className={"form-control " + this.state.password_error_class}
-                              placeholder="Enter password"
-                              value={this.state.password}
-                              name='password'
-                              onChange={this.onFormValueChangeHandler} />
-                          </div>
-
-
-                        </Col>
-                        <Col>
-                          <div className="form-group">
-                            <label>Confirm Password</label>
-                            <input type="password"
-                              required
-                              className={"form-control " + this.state.password_error_class}
-                              placeholder="Enter password"
-                              value={this.state.c_password}
-                              name='c_password'
-                              onChange={this.onFormValueChangeHandler}
-                            />
-                          </div>
-                        </Col>
-                      </Row>
-
-
-                      <span>
-                        <input type="checkbox"
-                          required
-                          checked={this.state.agreed}
-                          name='agreed'
-                          onChange={this.onFormValueChangeHandler} />
-                        <span className="forgot-password text-left"> I agree to the <a href="#"> Terms & Conditions</a> mentioned by Event Planners</span>
-
-                      </span>
-
-
-                      <button type="submit" className="btn btn-primary btn-block mt-3" disabled={!this.state.agreed || this.state.password_error_class != ''} onClick={this.registerUser}>Register</button>
-                      <p className="forgot-password text-right">
-                        Already registered <a href="#">sign in?</a>
-                      </p>
-        </container>
-      </form>
-
+      <div>
+       {pageContent}
+      </div>
     )
   }
 }
