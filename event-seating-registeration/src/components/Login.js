3ad934c8c761ff2495815d4edf9cee15aaa50a14
MemonaSultan <memonasultan54@gmail.com>
2020-03-26 01:14:12 +0500

    Login Form Fixes

--- event-seating-registeration/src/components/Login.js
+++ event-seating-registeration/src/components/Login.js
@@ -11,15 +11,15 @@ export default class LoginForm extends Component {
     super(props)
     this.state = {
       email: '',
-      password: ''
+      password: '',
+      loginClicked:false
     }
   }
 
-  componentWillMount(){
-    let user =  sessionStorage.getItem("user");
-    if(user)
-    {
-        this.props.history.push("/events");
+  componentWillMount() {
+    let user = sessionStorage.getItem("user");
+    if (user) {
+      this.props.history.push("/events");
     }
   }
 
@@ -29,83 +29,102 @@ export default class LoginForm extends Component {
     this.setState({ [name]: text });
   }
 
+
+  checkValidations = () => {
+
+    var { email, password } = this.state;
+    if (
+      email.trim() != "" &&
+      password.trim() != "") {
+      return true;
+    }
+    return false;
+
+
+  }
+
   registerUser = e => {
     e.preventDefault();
+    if (!this.checkValidations()) {
+      ToastsStore.warning("Fill the required information please")
+      return;
+    }
+
     let user = {
       email: this.state.email,
       password: this.state.password
 
     };
+    this.setState({ loginClicked: true });
+    axios.post(BASE_URL + "login",
+      user).then(data => {
+        console.log(data)
+        if (data.data.success) {
+          ToastsStore.success("Successfully Logged In!")
+          sessionStorage.setItem("user", JSON.stringify(data.data.data));
+          this.props.history.push("/events");
+        }
+        else {
+          this.setState({ loginClicked: false });
+          ToastsStore.error(data.data.message)
+        }
+      }).catch(err => {
+        console.log(err)
+        this.setState({ loginClicked: false });
+        ToastsStore.error("Something went wrong! Try Again..")
+      })
 
-    axios.post(BASE_URL + "login", 
-    user).then(data => {
-     console.log(data)
-     if (data.data.success) {
-       ToastsStore.success("Successfully Logged In!")
-       sessionStorage.setItem("user", JSON.stringify(data.data.data));
-       this.props.history.push("/events");
-     }
-     else {
-       ToastsStore.error(data.data.message)
-       this.setState({ showSeatPickerButton: false })
-     }
-   }).catch(err => {
-     console.log(err)
-     ToastsStore.error("Something went wrong! Try Again..")
-     this.setState({ showSeatPickerButton: false })
-   })
-  
   }
 
-  
-  goToTablePage = e =>{
+
+  goToTablePage = e => {
     return <Redirect to="/seatPicker"></Redirect>
   }
 
-  registrationForm(){
-   return  <div>
-    <form>
-      <h3>Login</h3>
-      <Row>
-        <Col>
-          <div className="form-group">
-            <label>Email address</label>
-            <input type="email"
-              required
-              className="form-control"
-              placeholder="Enter email"
-              value={this.state.email}
-              name='email'
-              onChange={this.onFormValueChangeHandler} />
-          </div>
-        </Col>
-       
-      </Row>
-      <Row>
-      <Col>
-          <div className="form-group">
-            <label>Password</label>
-            <input type="password"
-              required
-              className={"form-control " + this.state.password_error_class}
-              placeholder="Enter password"
-              value={this.state.password}
-              name='password'
-              onChange={this.onFormValueChangeHandler} />
-          </div>
+  registrationForm() {
+    return <div>
+      <form>
+        <h3>Login</h3>
+        <Row>
+          <Col>
+            <div className="form-group">
+              <label>Email address</label>
+              <input type="email"
+                required
+                className="form-control"
+                placeholder="Enter email"
+                value={this.state.email}
+                name='email'
+                onChange={this.onFormValueChangeHandler} />
+            </div>
+          </Col>
+
+        </Row>
+        <Row>
+          <Col>
+            <div className="form-group">
+              <label>Password</label>
+              <input type="password"
+                required
+                className={"form-control " + this.state.password_error_class}
+                placeholder="Enter password"
+                value={this.state.password}
+                name='password'
+                onChange={this.onFormValueChangeHandler} />
+            </div>
 
 
-        </Col>
+          </Col>
         </Row>
 
 
-      <button type="submit" className="btn btn-primary btn-block mt-3"  disabled={this.state.email=='' && this.state.password==''} onClick={this.registerUser}>Login</button>
-      <p className="forgot-password text-right">
-        No Account?  <span className="clickable" onClick={()=> this.props.history.push("/register")}>sign up?</span>
-      </p>
-    </form>
+        <button type="submit" className="btn btn-primary btn-block mt-3" disabled={this.state.email == '' && this.state.password == '' || this.state.loginClicked} onClick={this.registerUser}>Login</button>
+        <p className="forgot-password text-right">
+          No Account?  <span className="clickable" onClick={() => this.props.history.push("/register")}>sign up?</span>
+        </p>
+      </form>
 
-  </div>
+    </div>
   }
 
 
@@ -114,7 +133,7 @@ export default class LoginForm extends Component {
 
     return (
       <div>
-       {pageContent}
+        {pageContent}
       </div>
     )
   }
