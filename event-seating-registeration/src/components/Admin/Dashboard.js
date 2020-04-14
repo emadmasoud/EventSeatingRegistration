6ebe7ba4e6964f99a34b5add9b8e6aa26b32e099
MemonaSultan <memonasultan54@gmail.com>
2020-04-15 02:38:25 +0500

    formatted

--- event-seating-registeration/src/components/Admin/Dashboard.js
+++ event-seating-registeration/src/components/Admin/Dashboard.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Grid, Card, Image, Icon, Dropdown,Button , Tab} from 'semantic-ui-react';
+import { Grid, Card, Image, Icon, Dropdown, Button, Tab } from 'semantic-ui-react';
 import { connect } from "react-redux";
 import DataService from "../../services/dataService";
 import Header from '../Header';
@@ -20,39 +20,38 @@ class Dashboard extends Component {
             paidUsers: 0,
             availableTables: 0,
             registeredTables: 0,
-            reservedTables:0,
-      
+            reservedTables: 0,
+
             adminEvents: [],
             adminEventsOptions: [],
             users: [],
-            selectedEventId:0,
-            selectedEvent:{id:0},
-            allRegisteredUsers:[],
-            paidUsersList:[]
+            selectedEventId: 0,
+            selectedEvent: { id: 0 },
+            allRegisteredUsers: [],
+            paidUsersList: []
 
         }
     }
 
-    updateCount=(event)=>
-    {
+    updateCount = (event) => {
         console.log("UPDATE INFO CALLED")
         DataService.Instance.fetchAllEvents().then(evList => {
-                
-            event = evList.filter(ev=> ev.id == event.id)[0];
+
+            event = evList.filter(ev => ev.id == event.id)[0];
             DataService.Instance.fetchPaidUsers(event.id).then(users => {
-            
+
                 this.setState({
-                    selectedEvent:event,
+                    selectedEvent: event,
                     selectedEventId: event.id,
                     paidUsers: users.length,
                     availableTables: event.available_tables,
                     reservedTables: event.no_of_tables - event.available_tables,
                     paidUsersList: users
-                  })
+                })
             })
 
         })
-       
+
     }
     componentWillMount() {
         let user = JSON.parse(sessionStorage.getItem("user")).user;
@@ -60,112 +59,114 @@ class Dashboard extends Component {
             this.props.history.push("/login");
         } else {
 
-            
+
 
             DataService.Instance.fetchAllEvents().then(evList => {
-                
+
                 let options = evList.map(e => {
                     return {
                         key: e.id,
                         text: e.name,
                         value: e.id,
-                        label: e.isActive? { color: 'green', empty: true, circular: true }:{ color: 'red', empty: true, circular: true }
-                  
+                        label: e.isActive ? { color: 'green', empty: true, circular: true } : { color: 'red', empty: true, circular: true }
+
                     }
                 });
 
-                this.setState({ adminEventsOptions: options , adminEvents: evList})
+                this.setState({ adminEventsOptions: options, adminEvents: evList })
             })
 
             DataService.Instance.fetchUsers().then(evList => {
-                this.setState({ allRegisteredUsers: evList})
+                this.setState({ allRegisteredUsers: evList })
             })
 
-         
+
 
         }
     }
 
     handleChange = (e, { value }) => {
-        
-       
-        let event = this.state.adminEvents.filter(ev=> ev.id == value)[0];
+
+
+        let event = this.state.adminEvents.filter(ev => ev.id == value)[0];
 
 
         this.updateCount(event)
-    
+
     }
 
-    stopRegistration = () =>{
+    stopRegistration = () => {
         let eventID = this.state.selectedEventId;
         swal({
             title: "Are you sure?",
             text: "It will make the event unavailable for all users",
             icon: "warning",
-         
-          })
-          .then(willDelete => {
-            if (willDelete) {
-                DataService.Instance.stopRegistration(eventID).then(res=>{
-                    DataService.Instance.fetchAllEvents().then(evList => {
-                
-                        let options = evList.map(e => {
-                            return {
-                                key: e.id,
-                                text: e.name,
-                                value: e.id,
-                                label: e.isActive? { color: 'green', empty: true, circular: true }:{ color: 'red', empty: true, circular: true }
-                          
-                            }
-                        });
-        
-                        this.setState({ adminEventsOptions: options , adminEvents: evList})
+
+        })
+            .then(willDelete => {
+                if (willDelete) {
+                    DataService.Instance.stopRegistration(eventID).then(res => {
+                        DataService.Instance.fetchAllEvents().then(evList => {
+
+                            let options = evList.map(e => {
+                                return {
+                                    key: e.id,
+                                    text: e.name,
+                                    value: e.id,
+                                    label: e.isActive ? { color: 'green', empty: true, circular: true } : { color: 'red', empty: true, circular: true }
+
+                                }
+                            });
+
+                            this.setState({ adminEventsOptions: options, adminEvents: evList })
+                        })
+                        swal({
+                            title: "Success!",
+                            text: "Registration Stopped",
+                            icon: "success"
+                        })
                     })
-                    swal({
-                        title: "Success!",
-                        text: "Registration Stopped",
-                        icon: "success"      
-                      })
-                })
-            }
-          });
-       
+                }
+            });
+
     }
 
-   
+
 
     render() {
 
         const panes = [
-            { menuItem: 'Event Tables Info', render: () => <Tab.Pane>
-                
-                        <EventManagement event={this.state.selectedEvent}  allUsers={this.state.allRegisteredUsers} updateCount={this.updateCount}></EventManagement>
-                        
-            </Tab.Pane> },
+            {
+                menuItem: 'Event Tables Info', render: () => <Tab.Pane>
+
+                    <EventManagement event={this.state.selectedEvent} allUsers={this.state.allRegisteredUsers} updateCount={this.updateCount}></EventManagement>
+
+                </Tab.Pane>
+            },
             { menuItem: 'Event Paid Users Info', render: () => <Tab.Pane><UserManagement event={this.state.selectedEvent} allUsers={this.state.allRegisteredUsers} allPaidUsers={this.state.paidUsersList}></UserManagement></Tab.Pane> }
-          ]
+        ]
 
         return (
             <div className="">
                 <Header></Header>
-               
-                <Grid style={{ textAlign: '',marginLeft: '70px' }}>
+
+                <Grid style={{ textAlign: '', marginLeft: '70px' }}>
                     <Grid.Row columns={2}>
-                  
+
                         <Grid.Column>
                             <Dropdown
                                 placeholder='Select Event to see details'
                                 fluid
                                 selection
                                 options={this.state.adminEventsOptions}
-                                style={{width:'400px', display:"inline-block", marginRight:'30px'}}
-                                onChange={this.handleChange}                        
+                                style={{ width: '400px', display: "inline-block", marginRight: '30px' }}
+                                onChange={this.handleChange}
                             />
-                           <Button onClick={this.stopRegistration} style={{backgroundColor:'red', color:'white'}}>Stop Registration</Button>
-                            
+                            <Button onClick={this.stopRegistration} style={{ backgroundColor: 'red', color: 'white' }}>Stop Registration</Button>
+
                         </Grid.Column>
-                        
-                       
+
+
                     </Grid.Row>
                     <Grid.Row>
                         <Grid.Column md={3}>
@@ -213,20 +214,20 @@ class Dashboard extends Component {
                                 </Card.Content>
                             </Card>
                         </Grid.Column>
-                        
+
                     </Grid.Row>
 
                     <Grid.Row columns={'sixteen'}>
-                     
-                    {this.state.selectedEventId?
-                        <Tab panes={panes}>
 
-                        </Tab>:''}
-                       
-                        
-                      
+                        {this.state.selectedEventId ?
+                            <Tab panes={panes}>
+
+                            </Tab> : ''}
+
+
+
                     </Grid.Row>
-                   
+
                 </Grid>
 
 
